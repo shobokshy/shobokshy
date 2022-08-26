f12f00c7f5f21566c738a2664b37d7f87bf0bd40
shobokshy <>
2022-08-26 20:29:41 +1000

    fix: global styles not working in ssr

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,21 @@
-import { getCssText } from "@cayro/ui-primitives";
+import { defaultStyle, getCssText, globalCss } from "@cayro/ui-primitives";
 import { Head, Html, Main, NextScript } from "next/document";
 
+const globalStyles = globalCss({
+	"*": { margin: 0, padding: 0 },
+	html: {
+		scrollBehavior: "smooth",
+	},
+	body: {
+		fontSize: "100%",
+		fontFamily: "$body",
+		color: "$text",
+		backgroundColor: "$background",
+	},
+});
+
 export default function Document() {
+	globalStyles();
 	return (
 		<Html lang="en">
 			<Head>
@@ -11,7 +25,7 @@ export default function Document() {
 
 				<style id="cayro-ui" dangerouslySetInnerHTML={{ __html: getCssText() }} />
 			</Head>
-			<body>
+			<body className={defaultStyle.darkScheme.theme.className}>
 				<Main />
 				<NextScript />
 			</body>
