e678cca885db89be9fce045096637a7a19e18b9d
shobokshy <>
2022-09-02 17:03:38 +1000

    feat: added some security headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
 const withTM = require("next-transpile-modules")(["@cayro/ui-core", "@cayro/ui-primitives", "@cayro/ui-icons"]);
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'unsafe-eval';
+  style-src 'unsafe-inline';
+  font-src 'self';
+  img-src 'self';
+  connect-src 'self';
+  object-src 'self';
+  media-src 'self';
+  frame-src 'self';
+  child-src 'self';
+  base-uri 'self';
+  script-src-elem 'self';
+`;
+
 module.exports = withTM({
 	reactStrictMode: false,
 	webpack: (config) => {
@@ -9,5 +24,46 @@ module.exports = withTM({
 		});
 
 		return config;
+	},
+	async headers() {
+		return [
+			{
+				source: "/(.*)",
+				headers: [
+					{
+						key: "Content-Security-Policy",
+						value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+					},
+					{
+						key: "X-DNS-Prefetch-Control",
+						value: "on",
+					},
+					{
+						key: "Strict-Transport-Security",
+						value: "max-age=63072000; includeSubDomains; preload",
+					},
+					{
+						key: "Server",
+						value: "Apache",
+					},
+					{
+						key: "X-Content-Type-Options",
+						value: "nosniff",
+					},
+					{
+						key: "X-Frame-Options",
+						value: "DENY",
+					},
+					{
+						key: "X-XSS-Protection",
+						value: "1; mode=block",
+					},
+					{
+						key: "Referrer-Policy",
+						value: "same-origin",
+					},
+				],
+			},
+		];
 	},
 });
