02a844c93ce8f11557d3ef8649c8426cf2ea0c2b
shobokshy <>
2022-09-04 09:01:05 +1000

    fix: move title to _app as per nextjs warning

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,21 @@ import { SSRProvider, ThemeProvider } from "@cayro/ui-core";
 import { ColorScheme } from "@cayro/ui-primitives";
 import "@cayro/ui-primitives/dist/fonts/fonts.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import { Fragment } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<SSRProvider>
-			<ThemeProvider preferdColorScheme={ColorScheme.Dark as any}>
-				<Component {...pageProps} />
-			</ThemeProvider>
-		</SSRProvider>
+		<Fragment>
+			<Head>
+				<title>shobokshy</title>
+			</Head>
+			<SSRProvider>
+				<ThemeProvider preferdColorScheme={ColorScheme.Dark as any}>
+					<Component {...pageProps} />
+				</ThemeProvider>
+			</SSRProvider>
+		</Fragment>
 	);
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,10 +21,7 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
-				<title>shobokshy</title>
-
 				<meta name="theme-color" content="#000" />
-
 				<style id="cayro-ui" dangerouslySetInnerHTML={{ __html: getCssText() }} />
 			</Head>
 			<body className={darkThemeClassName}>
